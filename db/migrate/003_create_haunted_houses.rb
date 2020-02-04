ceb77ef697b26780894945e957255832b95b8dcc
David Cantoran <davidcantoran36@gmail.com>
2020-02-04 16:46:59 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,6 +6,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
     create_table :haunted_houses do |t|
       t.string :name 
       t.string :location
+      t.string :theme
       t.
     end 
   end 
